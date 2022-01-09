8b46615f591fb3395896f994f861fe49df63f03e
Philip Beauford <PhilipBeauford@github.com>
2022-01-08 21:49:40 -0800

    finalized

--- README.md
+++ README.md
@@ -1,19 +1,13 @@
-# Bare Next.js + Tailwind CSS
-Starter NextJS, TailwindCSS & TypeScript blank canvas!
-Load it up and go!
+# Philips NextJS Pokedex
+Pokedex utilizing NextJS & TailwindCSS!
 
-## Deploy your own
-
-Deploy the example using [Vercel](https://vercel.com?utm_source=github&utm_medium=readme&utm_campaign=next-example):
-
-[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https://github.com/vercel/next.js/tree/canary/examples/with-tailwindcss&project-name=with-tailwindcss&repository-name=with-tailwindcss)
 
 ## How to use
 
 
 ```bash
 npm install
-# 
+#
 npm run dev
 ```
 
---
+++ pages/index.jsx
@@ -0,0 +1,57 @@
+import Head from 'next/head'
+import Layout from '../components/Layout'
+import Link from 'next/Link'
+
+
+export default function Home({pokemon}) {
+  console.log(pokemon)
+
+  return (
+    <Layout title= "Philips NextJS Pokedex">
+
+        <h1 className='text-4xl mb-8 text-center text-white'>
+          Philips NextJS Pokedex!
+        </h1>
+        <ul>
+          {pokemon.map((pokeman, index) => (
+            <li key={index}>
+                <Link href={`/pokemon?id=${index +1}`}>
+                  <a className='border p-4 border-gray my-2 capitalize flex items-center text-lg bg-gray-200 rounded-2xl'>
+                    <img className='w-20 h-20 mr-3' src={pokeman.image} alt={pokeman.name} />
+                    <span className='mr-2 font-bold'>{index +1}.</span>
+                    {pokeman.name}
+                  </a>
+                </Link>
+            </li>
+          ))}
+        </ul>
+      
+    </Layout>
+  )
+}
+
+export async function getStaticProps(context){
+  try {
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
+    const { results } = await res.json()
+    const pokemon = results.map((result, index) => {
+      
+      const paddedIndex = ('00' + (index + 1)).slice(-3)  //001, 002 etc
+      const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedIndex}.png`
+
+      return {
+        ...result,
+        image,
+      }
+    })
+
+    return {
+      props: {pokemon}
+    }
+
+  } catch (err) {
+    console.error(err)
+  }
+
+  
+}
